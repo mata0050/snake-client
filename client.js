b977787faeb379641dc28c70ec7605fed33e18d3
mata0050 <mata0050@outlook.com>
2021-12-01 17:09:43 -0500

    display name

--- client.js
+++ client.js
@@ -13,6 +13,12 @@ const connect = function () {
   conn.on('data', (data) => {
     console.log(data);
   });
+
+  conn.on('connect', (socket) => {
+    console.log('Successfully connected to game server');
+  });
+
+  conn.write('Name: MTM');
   return conn;
 };
 
