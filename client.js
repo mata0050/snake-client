8900f12604a0969a060844f668078744a8b12a2e
Michael Matambo <58061791+mata0050@users.noreply.github.com>
2021-12-01 16:51:28 -0500

    finished

---
+++ client.js
@@ -0,0 +1,8 @@
+const net = require('net');
+const connect = require('./client');
+
+console.log('Connecting ...');
+connect();
+
+// start an instance of a tcp server
+// const server = net.createServer();
