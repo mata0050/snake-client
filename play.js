b4615c41925bda08558766dcd7695c2dc22e2694
Michael Matambo <58061791+mata0050@users.noreply.github.com>
2021-12-01 16:52:06 -0500

    Update play.js

--- play.js
+++ play.js
@@ -1,24 +1,9 @@
 const net = require('net');
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '10.0.0.142',
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-
-  // getting data from server
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-  return conn;
-};
+const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
 
 // start an instance of a tcp server
 // const server = net.createServer();
+
